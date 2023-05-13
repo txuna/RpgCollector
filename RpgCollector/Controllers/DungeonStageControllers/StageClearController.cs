a26ed62ecfda0729923d5239d586115492e2a117
tuuna <b7712983@naver.com>
2023-05-13 17:41:57 +0900

    Update StageClearController.cs

--- RpgCollector/Controllers/DungeonStageControllers/StageClearController.cs
+++ RpgCollector/Controllers/DungeonStageControllers/StageClearController.cs
@@ -21,6 +21,16 @@ namespace RpgCollector.Controllers.DungeonStageControllers
         [HttpPost]
         public async Task<StageClearResponse> Index(StageClearRequest stageClearRequest)
         {
+            // 클리어 검증 
+
+            // 유저 상태 변경 
+
+            // 던전 내용 백업 및 삭제 
+
+            // 경험치 보상 및 파밍 아이템 제공
+
+            // 현재 스테이지 ID와 디베이 저장된 스테이지 ID와 비교하여 같으면 +1 
+
             return new StageClearResponse
             {
                 Error = RequestResponseModel.ErrorCode.None
